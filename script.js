4c899a81d1f69b23ee1adb2048b80e57e647f3eb
Luciana Ridao Martin <ridao.luciana@gmail.com>
2022-06-28 14:21:39 -0300

    primer commit, entrega de primer desafio

---
+++ script.js
@@ -0,0 +1,11 @@
+
+let Cuentaregresiva =10
+
+console.log("comienza la cuenta regresiva para año nuevo")
+
+while(Cuentaregresiva !=0){
+    console.log("Cuenta Regresiva:"+ Cuentaregresiva + "...")
+    Cuentaregresiva = Cuentaregresiva -=1
+}
+
+console.log("Feliz Año Nuevo")
