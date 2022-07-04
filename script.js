d3ff1cef65502ad22ea1892d4dcc05704ebb95b9
Luciana Ridao Martin <ridao.luciana@gmail.com>
2022-07-04 00:16:13 -0300

    desafio dos

--- script.js
+++ script.js
@@ -1,11 +1,48 @@
+function buscarAmigos (nombre){
+    return `Hola ${nombre}, agrega el nombre de la persona que quieras saber el cumpleños`
+    }
+    
+    console.log(buscarAmigos("Lula"))
 
-let Cuentaregresiva =10
 
-console.log("comienza la cuenta regresiva para año nuevo")
 
-while(Cuentaregresiva !=0){
-    console.log("Cuenta Regresiva:"+ Cuentaregresiva + "...")
-    Cuentaregresiva = Cuentaregresiva -=1
+let nombreCumplañero, nombre, apellido
+
+do{
+    nombreCumplañero=prompt("ingrese nombre").toLowerCase()
 }
 
-console.log("Feliz Año Nuevo")
+
+while(nombreCumplañero == "")
+
+let cumpleañosAmigos
+
+switch (nombreCumplañero) {
+    case "pablo zunino": 
+     alert (`16 de Abril del 2022`)
+     break
+    case "luciana ridao":
+        alert (`11 de Julio del 2022` )
+    break
+
+    case "monica martin":
+    alert (`21 de diciembre del 2022`)
+    break
+
+    case "agustina ridao":
+        alert (`20 de diciembre del 2022`)
+
+        case "alejandro ridao":
+            alert (`6 de septiembre del 2022`)
+
+    default:
+        alert(`su nombre es incorrecto`)
+
+       
+}
+
+function resultadoBusqueda (){
+    return `comparte esta aplicacion para estar al tanto de las fechas importantes`
+    }
+    
+    console.log(resultadoBusqueda())
